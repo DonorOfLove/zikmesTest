864ff1405817591487d3f4b54a8493a0aa856a55
DonorOfLove <StasEzovitov89@gmail.com>
2022-11-14 19:30:12 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import s from './input.module.sass'
+import {useRef,useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCoffee} from "@fortawesome/free-solid-svg-icons/faCoffee";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const ref = useRef()
+
+    function submit() {
+        setOperator('')
+        setProgress('')
+        setValid('')
+        if (ref.current.value){
+            fetch(`https://api.regius.name/iface/phone-number.php?phone=${ref.current.value}&token=8bec50f53cefa97e54659092c54c66fdeedf0adb`)
+                .then(response=>response.json())
+                .then(json=>setOperator(json))
+                .catch(er=>setProgress('error'))
+        }else {
+            setValid('empty field')
+        }
+    }
+
+    const [valid, setValid] = useState('');
+    const [progress, setProgress]=useState('');
+    const [operator,setOperator]=useState('')
+    return (
+    <div className={s.main}>
+        <div className={s.search__input__wrap}>
+                <input type="text" placeholder='Ваш номер' ref={ref}/>
+        </div>
+        <div className={s.btn}>
+            <button>
+                <div className={s.btn_text} onClick={()=>submit()}>ЗАКАЗАТЬ <FontAwesomeIcon icon={faCoffee} /></div>
+            </button>
+        </div>
+        <h1>{valid}</h1>
+        <h1>{operator.brand?(operator.brand):(progress)}</h1>
     </div>
   );
 }
